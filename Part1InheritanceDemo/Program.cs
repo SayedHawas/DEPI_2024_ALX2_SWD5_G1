131a575202854d72de7e1289e209c9f32a442214
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2024-12-06 14:28:39 +0200

    Add project files.

---
+++ Part1InheritanceDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part1InheritanceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
