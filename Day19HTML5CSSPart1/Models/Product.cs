050e0337038980146e82caf7beedbcbc2745e2fa
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2024-12-30 22:11:41 +0200

    Update Day 19 Css3 Day 3

---
+++ Day19HTML5CSSPart1/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day19HTML5CSSPart1.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; } = string.Empty; // equal ""
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+
+    }
+}
