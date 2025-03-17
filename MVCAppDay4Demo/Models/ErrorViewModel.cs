97bf5abae2881c675aa17569f415814b07510ae5
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-03-17 22:02:14 +0200

    Update

---
+++ MVCAppDay4Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCAppDay4Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
