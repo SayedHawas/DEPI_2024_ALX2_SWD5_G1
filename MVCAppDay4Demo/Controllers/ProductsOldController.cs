4b9b6311c15198f4c0e03a5e4e0818ed7adb1544
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-03-21 17:10:14 +0200

    Update

---
+++ MVCAppDay4Demo/Controllers/ProductsOldController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAppDay4Demo.Controllers
+{
+    public class ProductsOldController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        //Ctor
+        public ProductsOldController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        // Index --> List  
+        public async Task<IActionResult> Index()
+        {
+            var products = await _appDbContext.Products.ToListAsync();
+            return View(products);
+        }
+        //public 
+        //View To Open View 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Create Post (Insert Data)
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            //Validation Serve Side 
+            if (ModelState.IsValid)
+            {
+                //Save 
+                _appDbContext.Products.Add(product);
+                await _appDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+
+
+
+    }
+}
